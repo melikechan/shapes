b10d18b49a538ceaa0851bfbd0d5c1f76bdaeaa4
Melike VURUCU <41804132+melikechan@users.noreply.github.com>
2020-11-19 09:57:46 +0300

    Update num_square.cpp

--- num_square.cpp
+++ num_square.cpp
@@ -6,7 +6,7 @@
 using namespace std;
 
 void numSquare (int num) {
-  int colWidth = (int)log10(num) + 1; 
+  int colWidth = (int)log10(num) + 2; 
   int minNum = num, col, row;
   for (col = 0; col < num; ++col) {
     minNum = num - col;
